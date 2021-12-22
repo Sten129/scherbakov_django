3ba3aa7e20492cb19b2f06ce74ef0cfc20e081c4
Дмитрий Грицай <dgritsay@inbox.ru>
2021-12-22 22:34:11 +0300

    Prototyping - add AdminPanel classes

--- backend/admin.py
+++ backend/admin.py
@@ -2,42 +2,94 @@ from django.contrib import admin
 from .models import Picture, Photo, Letter, Document, Exhibition, Genre, Book, Persone, Owner, Technic, Type, Location
 
 class TypeAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class PictureAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class LocationAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class GenreAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class LocationAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class ExhibitionAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class TechnicAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class OwnerAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class PersoneAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class BookAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class DocumentAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class LetterAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 class PhotoAdmin(admin.ModelAdmin):
+    list_display = ()
+    empty_value_display = '-пусто-'
+    search_fields = ()
+    list_filter = ()
     pass
 
 
