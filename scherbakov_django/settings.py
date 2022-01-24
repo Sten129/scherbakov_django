36f6a61c4e9e7471148ce803e029b4a8ad89d11f
Дмитрий Грицай <dgritsay@inbox.ru>
2022-01-24 20:59:25 +0300

    Unstable 1.7, dockerfiles added - workflow attemp1

--- scherbakov_django/settings.py
+++ scherbakov_django/settings.py
@@ -83,13 +83,22 @@ WSGI_APPLICATION = 'scherbakov_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': 'Svinoskot',
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
