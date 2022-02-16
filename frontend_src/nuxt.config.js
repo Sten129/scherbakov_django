e0ce3ee6e0cf504b80e5522437d6a4732deac632
Ad Ok <adolf.oknov@gmail.com>
2022-02-16 06:10:30 +0300

    add PRODUCTION and API_TARGET params and make it work in config and component

--- frontend_src/nuxt.config.js
+++ frontend_src/nuxt.config.js
@@ -1,102 +1,108 @@
-export default {
+export default () => {
+  const publicRuntimeConfig = {
+    PRODUCTION: process.env.PRODUCTION || 'true',
+    API_TARGET: process.env.API_TARGET || 'localhost'
+  }
+  const privateRuntimeConfig = {}
 
-  server: {
-    host: '0' // default: localhost
-  },
-  publicRuntimeConfig: {},
-  privateRuntimeConfig: {},
+  return {
+    publicRuntimeConfig,
+    privateRuntimeConfig,
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'scherbakov',
-    htmlAttrs: {
-      lang: 'en'
+    ...process.env.PRODUCTION == 'true' && { server: { host: '0'} },
+
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+      title: 'scherbakov',
+      htmlAttrs: {
+        lang: 'en'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+      // https://go.nuxtjs.dev/typescript
+      '@nuxt/typescript-build',
+      // https://go.nuxtjs.dev/tailwindcss
+      '@nuxtjs/tailwindcss'
+    ],
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss'
-  ],
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://go.nuxtjs.dev/axios
+      '@nuxtjs/axios',
+      '@nuxtjs/i18n'
+    ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    '@nuxtjs/i18n'
-  ],
-
-  i18n: {
-    locales:
-      process.env.AVAILABLE_LOCALES.split(',').map((l) => {
-      return { code: l, file: `${l}.js` }
-    }),
-    defaultLocale: process.env.DEFAULT_LOCALE,
-    langDir: 'i18n/',
-    vueI18n: {
-      fallbackLocale: process.env.DEFAULT_LOCALE
-    }
-  },
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {
-    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/'
-  },
-
-  tailwindcss: {
-    cssPath: '~/assets/css/tailwind.css'
-  },
-
-  loading: {
-    color: 'blue',
-    height: '5px'
-  },
-
-  buildDir: process.env.BUILD_DIR,
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    extend(config) {
-      config.module.rules.push({
-        test: /\.ya?ml$/,
-        use: 'js-yaml-loader'
-      })
+    i18n: {
+      locales:
+        process.env.AVAILABLE_LOCALES.split(',').map((l) => {
+        return { code: l, file: `${l}.js` }
+      }),
+      defaultLocale: process.env.DEFAULT_LOCALE,
+      langDir: 'i18n/',
+      vueI18n: {
+        fallbackLocale: process.env.DEFAULT_LOCALE
+      }
     },
-    postcss: {
-      // Add plugin names as key and arguments as value
-      // Install them before as dependencies with npm or yarn
-      plugins: {
-        // Disable a plugin by passing false as value
-        'tailwindcss/nesting': {},
-        'postcss-nested': {
-          bubble: ['screen']
-        },
-        'postcss-hexrgba': {},
-        tailwindcss: {}
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+      // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+      baseURL: '/'
+    },
+
+    tailwindcss: {
+      cssPath: '~/assets/css/tailwind.css'
+    },
+
+    loading: {
+      color: 'blue',
+      height: '5px'
+    },
+
+    buildDir: process.env.BUILD_DIR,
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+      extend(config) {
+        config.module.rules.push({
+          test: /\.ya?ml$/,
+          use: 'js-yaml-loader'
+        })
+      },
+      postcss: {
+        // Add plugin names as key and arguments as value
+        // Install them before as dependencies with npm or yarn
+        plugins: {
+          // Disable a plugin by passing false as value
+          'tailwindcss/nesting': {},
+          'postcss-nested': {
+            bubble: ['screen']
+          },
+          'postcss-hexrgba': {},
+          tailwindcss: {}
+        }
       }
     }
   }
