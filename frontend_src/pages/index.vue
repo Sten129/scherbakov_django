4c17920c294fc396d6eb1cdafe75bde177a04f67
Ad Ok <adolf.oknov@gmail.com>
2022-02-17 23:58:45 +0300

    Revert "add PRODUCTION and API_TARGET params and make it work in config and component"
    This reverts commit e0ce3ee6e0cf504b80e5522437d6a4732deac632.

--- frontend_src/pages/index.vue
+++ frontend_src/pages/index.vue
@@ -10,17 +10,13 @@ import Vue from 'vue'
 
 export default Vue.extend({
   name: 'IndexPage',
-  async asyncData({ $axios, $config }) {
-    // const persons = await $axios.$get('http://web:8000/api/persone/?format=json')
-    const persons = await $axios.$get(`${$config.API_TARGET}persone/?format=json`)
+  async asyncData({ $axios }) {
+    const persons = await $axios.$get('http://web:8000/api/persone/?format=json')
     return {
       loaded: true,
       persons,
       name: persons.results[0].name
      }
-  },
-  mounted() {
-    // console.log(this.$config)
   }
 })
 </script>
