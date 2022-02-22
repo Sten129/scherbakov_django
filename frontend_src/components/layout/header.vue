8dd3ef273504c3b69f6341144805e5ec81cc34cc
Ad Ok <adolf.oknov@gmail.com>
2022-02-22 17:20:43 +0300

    add main menu

--- frontend_src/components/layout/header.vue
+++ frontend_src/components/layout/header.vue
@@ -2,16 +2,34 @@
   .container
     .flex.w-full.justify-between
       nav
-        nuxt-link(:to="localePath('index')") Home
+        nuxt-link(:to="localePath('index')") {{ $t('menu.home') }}
+        nuxt-link(:to="localePath('pictures')") {{ $t('menu.pictures') }}
+        a(href="#") {{ $t('menu.exhibitions') }}
+        a(href="#") {{ $t('menu.books') }}
+        a(href="#") {{ $t('menu.people') }}
+        a(href="#") {{ $t('menu.documents') }}
+        a(href="#") {{ $t('menu.letters') }}
+        a(href="#") {{ $t('menu.provenance') }}
+        a(href="#") {{ $t('menu.articles') }}
+        a(href="#") {{ $t('menu.biography') }}
+
       LayoutLangs
+
 </template>
 
 <script>
 export default {
-  computed: {
-    availableLocales () {
-      return this.$i18n.locales.filter(i => i.code !== this.$i18n.locale)
-    }
-  }
+
 }
 </script>
+
+<style lang="postcss" scoped>
+  a {
+    @apply p-2;
+  }
+
+  .nuxt-link-exact-active {
+    @apply text-pink-500;
+  }
+
+</style>
---
+++ frontend_src/pages/pictures.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .container Pictures
+    pre {{ pictures }}
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const pictures = await $axios.$get('/picture/?format=json').then((result) => {
+      return result
+    }).catch((error) => {
+      console.log(error)
+    })
+    return {
+      loaded: true,
+      pictures,
+      list: pictures
+    }
+  }
+})
+</script>
