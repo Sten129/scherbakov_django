b2caa465f1f42795c9327652c3bbd9f66b88858e
Ad Ok <adolf.oknov@gmail.com>
2022-02-22 05:56:57 +0300

    add error layout

---
+++ frontend_src/layouts/error.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  .container
+    h2 Error
+    h2(v-if="error.statusCode === 404") 404
+    h2(v-else) {{ error.statusCode }}
+</template>
+
+<script lang="ts">
+import Vue from "vue"
+
+export default Vue.extend({
+  name: 'Error',
+  props: {
+    error: { type: Object, default: null }
+  }
+})
+</script>
